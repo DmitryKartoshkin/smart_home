e947520a2794836c609ab76fea5cccf22dc4bd25
Дима <mtt1@list.ru>
2022-12-23 14:40:31 +0300

    Program v 1.0

---
+++ logistic/views.py
@@ -0,0 +1,20 @@
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+from rest_framework import filters
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['title',]
+    search_fields = ["title", "description"]
+    # при необходимости добавьте параметры фильтрации
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
